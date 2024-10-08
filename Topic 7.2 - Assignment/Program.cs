8d3d55d4ac19a9d9cd75d7cc70af20d6771ce0b3
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-08 09:38:34 -0400

    Working

--- Topic 7.2 - Assignment/Program.cs
+++ Topic 7.2 - Assignment/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("1. Counting Down");
+for (int i = 10; i >= 0; i = i - 1)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Blast Off");
+Console.WriteLine();
+
+Console.WriteLine("2. X and Y");
+Console.WriteLine(" X\tY");
+Console.WriteLine("-----------");
+for (int i = -10; i <= 10; i = i + 2)
+{
+    Console.WriteLine(i + "\t" +  (i * i));
+}
+Console.WriteLine();
+
+Console.WriteLine("3. For Loop Fun");
